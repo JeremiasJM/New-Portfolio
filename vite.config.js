e1dc34007124513888de735ddcbf7b527d44467c
Jeremias Juarez Marrades <jeremiasjuarezmarrades15@hotmail.com>
2023-05-19 03:08:00 -0300

    confing vite 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://jeremiasjm.github.io/New-Portfolio/"
+  base: "https://JeremiasJM.github.io/New-Portfolio/"
 })
